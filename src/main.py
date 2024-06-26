92292c116cf71e10f231f646a93b1150563a16f4
Wesley <95096973+WesleySoftware@users.noreply.github.com>
2024-06-26 17:57:15 +0000

    Added argument parsing

--- src/main.py
+++ src/main.py
@@ -1,3 +1,29 @@
-import polars as pd
+import argparse
+import pgmpy
+import stix2
 
-print("hello world")
+
+def parse_args() -> argparse.Namespace:
+    """
+    Parse command line arguments.
+
+    Returns:
+        argparse.Namespace: Parsed arguments.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--hdm_file", type=str, required=True)
+    parser.add_argument("--flow_file", type=str)
+    parser.add_argument("--output_file", type=str, required=True)
+    return parser.parse_args()
+
+
+def main():
+    """
+    Main function of the program.
+    """
+    args = parse_args()
+    print(f"Hello, {args.name}!")
+
+
+if __name__ == "__main__":
+    main()
