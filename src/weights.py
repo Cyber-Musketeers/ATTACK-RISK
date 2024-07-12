29652fe2a1107df59d8a412aeab37ad9c8edb9e5
Wesley <95096973+WesleySoftware@users.noreply.github.com>
2024-07-11 22:29:44 -0400

    made connor's stuff slightly more readable

--- src/weights.py
+++ src/weights.py
@@ -1,20 +1,44 @@
-import sys
-
-# add local directory to system path
-sys.path.append("./")
-from attack_flow_extension import flow
-import argparse
-from typing import Any
-import pgmpy
-from collections import defaultdict
-import stix2
-import json
 from mitreattack.stix20 import MitreAttackData
 
 
-class TechniquesArray:
-    def __init__(self):
+class BayesianWeights:
+    def __init__(self, mitre_data: MitreAttackData):
         self.techniques = []
+        self.mitre_data = mitre_data
+        campaigns = mitre_data.get_all_techniques_used_by_all_campaigns()
+        self.number_of_campaigns = len(campaigns)
+
+        for campaign in campaigns:
+            attack_patterns = mitre_data.get_techniques_used_by_campaign(campaign)
+            for pattern in attack_patterns:
+                name = pattern.get("object").get("name")
+                stix_identifier = (
+                    pattern.get("object")
+                    .get("external_references")[0]
+                    .get("external_id")
+                )
+                self.add_or_increment_technique(stix_identifier)
+        self.techniques_with_probability = {}
+        techCounter = 0
+        for i in all_techniques:
+            name = i.get("name")
+            for ref in i["external_references"]:
+                if "external_id" in ref:
+                    external_id = ref["external_id"]
+                    count = self.get_technique_count(id)
+                    prob = count / self.number_of_campaigns
+                    attack_pattern = mitre_data.get_object_by_attack_id(
+                        external_id, "attack-pattern"
+                    )
+                    stixID = attack_pattern.get("id")
+                    self.techniques_with_probability[techCounter] = {
+                        "Name": name,
+                        "ID": external_id,
+                        "Count": count,
+                        "Probability": prob,
+                        "STIX ID": stixID,
+                    }
+            techCounter += 1
 
     def add_or_increment_technique(self, technique):
         # Check if the technique exists in the array
@@ -40,17 +64,17 @@ class TechniquesArray:
             ]
         )
 
-    def get_technique_count(self, technique: str) -> int:
+    def get_technique_count(self, technique: str) -> int | None:
         for item in self.techniques:
             if item["name"] == technique:
                 return item["counter"]
-        return 0  # Technique not found
+        return None  # Technique not found
 
-    def get_technique(self, technique: str) -> dict:
+    def get_technique(self, technique: str) -> dict | None:
         for item in self.techniques:
             if item["name"] == technique:
                 return item
-        return {}  # Technique not found
+        return None  # Technique not found
 
 
 # Example usage
@@ -58,45 +82,3 @@ mitre_data = MitreAttackData("enterprise-attack.json")
 
 
 all_techniques = mitre_data.get_techniques()
-# x = mitre_data.get_attack_id()
-# print(x)
-techCount = 0
-for tech in all_techniques:
-    techCount += 1
-print(techCount)
-
-tech_array = TechniquesArray()
-campaigns = mitre_data.get_all_techniques_used_by_all_campaigns()
-print(mitre_data)
-
-camCounter = 0
-
-for campaign in campaigns.keys():
-    camCounter += 1
-    attack_pattern = mitre_data.get_techniques_used_by_campaign(campaign)
-    for pattern in attack_pattern:
-        name = pattern.get("object").get("name")
-        id = pattern.get("object").get("external_references")[0].get("external_id")
-        tech_array.add_or_increment_technique(id)
-
-print(tech_array)
-tech_array_w_prob = {}
-techCounter = 0
-for i in all_techniques:
-    name = i.get("name")
-    for ref in i["external_references"]:
-        if "external_id" in ref:
-            id = ref["external_id"]
-            count = tech_array.get_technique_count(id)
-            prob = count / camCounter
-            attackPattern = mitre_data.get_object_by_attack_id(id, "attack-pattern")
-            stixID = attackPattern.get("id")
-            tech_array_w_prob[techCounter] = {
-                "Name": name,
-                "ID": id,
-                "Count": count,
-                "Probability": prob,
-                "STIX ID": stixID,
-            }
-    techCounter += 1
-print(tech_array_w_prob)
